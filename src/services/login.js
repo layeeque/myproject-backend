27f0cae43f82f48323615181ca6a45dc9c39e440
mdlayeeque urrehman <mdlayeeque.urrehman@ATMECS.CORP>
2018-09-05 15:57:38 +0530

    backend

---
+++ src/services/login.js
@@ -0,0 +1,74 @@
+const jwt = require('jsonwebtoken');
+const empModel = require('../models/login');
+
+
+
+
+module.exports =(req, res) => {
+    return {
+        login: (req, res) => {
+            console.log("I am in login services")
+
+            empModel.find({ 'name': req.body.name }, (err, data) => {
+                if (err) {
+                    res.status('400').send(err);
+                } else {
+                    console.log("value is " + data)
+                    if(data == ""){
+                        res.send({
+                            'success': '0',
+                            'message': 'Username/Password is Invalid'
+                            
+                        })
+
+                    }
+                    // else if (data[0].password != req.body.password) {
+                    //     res.status(403).send({
+                    //         'success': '0',
+                    //         'message': 'Username/Password is Invalid'
+                    //     })
+                    // }
+                     else {
+                        var name = data[0].name;
+                        var token = jwt.sign({ name }, 'secretkey', { expiresIn: '6000s' }).toString();
+                        var resObj = {};
+                        resObj['success'] = '1';
+                        resObj['token'] = token;
+                        resObj['data'] = data;
+                        res.send(resObj);
+                    }
+                }
+            })
+        },
+        signUp: (req, res) => {
+            console.log("in sign up model")
+            empModel.create(req.body, (err, data) => {
+                if (err) {
+                    res.status('400').send(err);
+                } else {
+                    
+                    var resObj = {};
+                    resObj['success'] = '1';
+                    resObj['message'] = 'saved successfully';
+                    res.send(resObj);
+                }
+            })
+        },
+        events: (req, res) => {
+            var resObj = {};
+            resObj['success'] = '1';
+            resObj['message'] = 'saved successfully';
+            res.send(resObj);
+        },
+        social:(req, res) => {
+            var name = req.body.name;
+            var token = jwt.sign({ name }, 'secretkey', { expiresIn: '6000s' }).toString();
+            var resObj = {};
+            resObj['success'] = '1';
+            resObj['token'] = token;
+            //resObj['data'] = data;
+            res.send(resObj);
+        }
+        
+    }
+}
